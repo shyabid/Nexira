cbb62902c1b84eb24ec8a6ff081b237b47a3114e
CyberXCore <209958457+CyberXCore@users.noreply.github.com>
2026-01-24 04:51:47 +0600

    Initial server setup

---
+++ api/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
+const { connectDB } = require("../db");
+
+const jobRouter = require("../routes/jobRouter");
+const taskRouter = require("../routes/taskRouter");
+const dashboardRouter = require("../routes/dashboardRouter");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server running on Vercel");
+});
+
+app.use("/jobs", jobRouter);
+app.use("/added-tasks", taskRouter);
+app.use("/dashboard", dashboardRouter);
+
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+};

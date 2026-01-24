09f629303afe9d364b05e9ddd1bec3943cc8c2a1
CyberXCore <209958457+CyberXCore@users.noreply.github.com>
2026-01-24 09:26:05 +0600

    fix dashboard router auth middleware

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-const validateTokenId = require("../middlewares/validateTokenId.js");
 const verifyTokenId = require("../middlewares/verifyTokenId.js");
 const { getDashboardStats } = require("../controllers/dashboardController.js");
 
 const dashboardRouter = express.Router();
 
-dashboardRouter.use(validateTokenId, verifyTokenId);
+// 🔐 Single, reliable auth middleware
+dashboardRouter.use(verifyTokenId);
 
+// 📊 Dashboard stats
 dashboardRouter.get("/stats", getDashboardStats);
 
 module.exports = dashboardRouter;
